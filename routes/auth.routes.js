04b0c9fac1062a1cffe6908778b4f6fb74945df1
OnishenkoEgor <egoronishenko@yandex.ru>
2022-05-03 18:35:27 +0300

    Add auth and login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -29,7 +29,7 @@ router.post('/register', [
         const user = new User({ email, password: hashedPass })
         await user.save()
 
-        res.status(201).json({ message: 'User created' })
+        res.status(200).json({ message: 'User created' })
     } catch (error) {
         res.status(400).json({ message: 'Catch fatal error', error })
     }
@@ -69,15 +69,8 @@ router.post('/login', [
         res.status(400).json({ message: 'Catch fatal error', error })
     }
 })
-router.post('/delete/', [], async (req, res) => {
+router.post('/delete', [], async (req, res) => {
     try {
-        // const errors = validationResult(req);
-        // if (!errors.isEmpty()) {
-        //     return res.status(400).json({ message: 'email not correct' })
-        // }
-
-        console.log(req.body.id);
-
         const user = await User.findByIdAndDelete(req.body.id);
         if (!user) {
             return res.status(400).json({ message: 'can not remove user' })
